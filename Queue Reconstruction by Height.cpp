4cd489ebf7ce0d604d3dc9458cdf61af9c76083c
Pankhuri Bhatnagar <46318882+pankhuri25@users.noreply.github.com>
2020-06-11 19:39:58 +0530

    DAY 6
    Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
    
    Note:
    The number of people is less than 1,100.
    
     
    Example
    Input:
    [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
    
    Output:
    [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]

---
+++ Queue Reconstruction by Height.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        
+        vector<vector<int>> ans;   
+        
+        sort(people.begin(), people.end(), [](vector<int>& a, vector<int>& b) 
+            {
+            return a[0] > b[0] || (a[0] == b[0] && a[1] < b[1]);
+        });
+             
+        for(auto& p: people)
+            ans.insert(ans.begin() + p[1], p);                
+        return ans; 
+    }
+};
