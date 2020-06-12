6cdcae818839eb232e559f806a4933905f87ed74
Pankhuri Bhatnagar <46318882+pankhuri25@users.noreply.github.com>
2020-06-12 23:52:31 +0530

    DAY 7 (2)

---
+++ Coin Change (Bottom up).cpp
@@ -0,0 +1,16 @@
+int change(int amount, vector<int>& coins) {
+         
+         vector<vector<int>> dp (coins.size()+1 , vector<int>(amount+1, 0));
+         dp[0][0]=1;
+         for(int i=1; i<=coins.size(); i++)
+         {
+             dp[i][0]=1;
+             for(int j=1; j<=amount; j++)
+             {
+                 dp[i][j]=dp[i-1][j];
+                 if(j-coins[i-1]>=0)
+                      dp[i][j]+= dp[i][j-coins[i-1]];
+             }
+         }
+         return dp[coins.size()][amount];
+     }
